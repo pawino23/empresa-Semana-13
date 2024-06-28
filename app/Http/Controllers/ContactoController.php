84324fe40745335a8e38af6aaaa05f62843d37ac
pawino23 <t052700920@unitru.edu.pe>
2024-06-28 08:50:12 -0500

    Migraciones a la base de datos del Contacto

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\MensajeRecibido;
+use App\Models\Contacto;
 
 class ContactoController extends Controller
 {
@@ -15,6 +16,7 @@ class ContactoController extends Controller
 
     public function store(Request $request)
     {
+        // Valida y obtén los datos del formulario
         $mensaje = $request->validate([
             'nombre' => 'required',
             'email' => 'required|email',
@@ -27,7 +29,18 @@ class ContactoController extends Controller
             'mensaje.required' => 'Ingresa el mensaje'
         ]);
 
+        // Guarda los datos en la base de datos usando el modelo Contacto
+        Contacto::create([
+            'nombre' => $mensaje['nombre'],
+            'email' => $mensaje['email'],
+            'asunto' => $mensaje['asunto'],
+            'mensaje' => $mensaje['mensaje'],
+        ]);
+
+        // Envía el correo electrónico
         Mail::to('t052700920@unitru.edu.pe')->send(new MensajeRecibido($mensaje));
+
+        // Retorna una respuesta al usuario
         return 'Mensaje Enviado correctamente';
     }
 }
---
+++ database/migrations/2024_06_28_132832_create_contacto_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacto', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('email');
+            $table->string('asunto');
+            $table->text('mensaje');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacto');
+    }
+}
